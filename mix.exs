26eca5bafda869ac925a3cd2d458ed85576c4e84
Fredrik Wärnsberg <fredrik@warnsberg.se>
2017-10-18 13:56:55 +0200

    0.3.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Forma.Mixfile do
     [
       app: :forma,
       name: "forma",
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env == :prod,
