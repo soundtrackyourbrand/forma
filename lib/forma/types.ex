8bf4d376286c70d9e3a8d0c0bb545b06aa2a799f
Fredrik Wärnsberg <fredrik@warnsberg.se>
2017-10-19 14:37:31 +0200

    Switch to ets

--- lib/forma/types.ex
+++ lib/forma/types.ex
@@ -1,23 +1,26 @@
 defmodule Forma.Types do
   use GenServer
 
-  def for(pid \\ __MODULE__, module, type) do
-          GenServer.call(pid, {:type, module, type})
-      end
+  def for(module, type) do
+    case :ets.lookup(__MODULE__, {module, type}) do
+      [] -> GenServer.call(__MODULE__, {:type, module, type})
+      [{{_, _}, spec} | _] -> spec
+    end
+  end
 
   def start_link(args) do
-    GenServer.start_link(__MODULE__, %{}, args)
+    GenServer.start_link(__MODULE__, :start, args)
   end
 
-  def init(initial) do
-    {:ok, initial}
+  def init(:start) do
+    name = :ets.new(__MODULE__, [:set, :protected, :named_table])
+    {:ok, name}
   end
 
-  def handle_call({:type, module, t}, _from, state) do
-    case Map.get(state, module) do
-      nil -> types = Forma.Typespecs.compile(module)
-      {:reply, Map.get(types, {module, t}), Map.merge(state, types)}
-      type -> {:reply, type, state}
-    end
+  def handle_call({:type, module, t}, _from, name) do
+    types = Forma.Typespecs.compile(module)
+    spec = Map.get(types, {module, t})
+    true = :ets.insert(name, {{module, t}, spec})
+    {:reply, spec, name}
   end
 end
